be31508c04913e456a39dbe148f51e90abc93923
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-09 00:23:43 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "lqEFQbgL#8LD-Ea5HfzdwzVfv7s_Ya0TiffN-r7cJ_mk98EIVFgY",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/YFtmhOw.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello 🤗 Vortex-X is alive don't fear, I am alive!!!",
+};
