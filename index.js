bcfdbd4133d4e76e81476dfdeffd51c9e92b8d7a
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-09 17:10:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+
 
 const ownerNumber = ['2349021506036'] // put your number for . owner
 
@@ -43,6 +43,11 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //========================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//===========================================
+
 console.log("Connecting Vortex-X 🤗...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
