f16f4f1b2782f372aff559dae7521ba958936bb8
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-09 16:51:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "86MnSawA#cNzfH9WEpLhKir205sbdxpyhZ92xzu99zXhlfJnYQi0",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/YFtmhOw.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello 🤗 Vortex-X is alive don't fear, I am alive!!!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MONGODDB: process.env.MONGODB || "enter mongodb here",
 };
