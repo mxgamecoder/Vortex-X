f1892b03eb2c0646b12ae3325f53e42f1ba4e037
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-10 21:37:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,4 +11,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/YFtmhOw.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello 🤗 Vortex-X is alive 🙂, I am alive!!!'",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "private",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
