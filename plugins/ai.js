29d7fc63a611b60da7c61a622599008b92c174ba
mxgamecoder <OlamilekanDamilaraa@gmail.com>
2024-12-10 21:41:42 +0100

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -7,7 +7,7 @@ cmd({
     alias: ["gpt","bot"], 
     react: "📑",
     desc: "ai chat.",
-    category: "main",
+    category: "search",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
